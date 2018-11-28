449e4eb6aba01df92abc4ea1038239c07361ec7d
Paul Schaefer <yeppaul2@gmail.com>
2018-11-27 19:15:02 -0500

    Updated weekday JS with user locale

--- source/javascripts/_day.js
+++ source/javascripts/_day.js
@@ -1,6 +1,6 @@
 // Localized weekday names, adapted from https://twitter.com/wesbos/status/1067534197903056901
 var days = Array.from({ length: 7 }, (x, index) => (
-	new Date(0, 0, index).toLocaleDateString('en-US', { weekday: 'long' })
+	new Date(0, 0, index).toLocaleDateString(navigator.language, { weekday: 'long' })
 ));
 
 document.getElementById('day').innerHTML = days[new Date().getDay()];
