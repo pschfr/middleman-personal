f5b0b609b4deb417f7399514cf5c88a9ef22900c
Paul Schaefer <yeppaul2@gmail.com>
2018-11-27 19:09:50 -0500

    Rewrote day of the week JS with localized weekday names, thanks @wesbos

--- source/javascripts/_day.js
+++ source/javascripts/_day.js
@@ -1,2 +1,6 @@
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+// Localized weekday names, adapted from https://twitter.com/wesbos/status/1067534197903056901
+var days = Array.from({ length: 7 }, (x, index) => (
+	new Date(0, 0, index).toLocaleDateString('en-US', { weekday: 'long' })
+));
+
 document.getElementById('day').innerHTML = days[new Date().getDay()];
