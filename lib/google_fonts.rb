878c8c25daad05415b32cc671493e755a70004cd
Paul Schaefer <yeppaul2@gmail.com>
2018-12-02 17:57:13 -0500

    Finished Google Fonts Helper!

--- lib/google_fonts.rb
+++ lib/google_fonts.rb
@@ -9,18 +9,22 @@ module Google_Fonts_Helper
 	CONFIG_FILE = File.read('data/global.json')
 	# and parse it as JSON...
 	CONFIG_DATA = JSON.parse(CONFIG_FILE)
-	# to find the selected font name.
-	FONT_NAME   = CONFIG_DATA['google_font_name']
+	# to find the selected font name and weight.
+	FONT_NAME   = CONFIG_DATA['google_fonts']['name']
+	FONT_WEIGHT = CONFIG_DATA['google_fonts']['weights']
 
 	# Outputs font name as class
 	def google_font_class_name
-		# puts FONT_NAME.downcase
-		puts FONT_NAME.parameterize
+		return FONT_NAME.parameterize
 	end
 
 	# Outputs proper URL for the Google Fonts CSS
 	def google_fonts_stylesheet_url
-		# puts GOOGLE_APIS + FONT_NAME.parameterize(separator: '+')
-		puts GOOGLE_APIS + FONT_NAME.gsub!(' ', '+')
+		return "#{GOOGLE_APIS}#{FONT_NAME.gsub(' ', '+')}:#{FONT_WEIGHT}"
+	end
+
+	# Outputs CSS class for Tachyons-like usage
+	def google_fonts_CSS_class
+		return "<style>.#{FONT_NAME.parameterize}{font-family:'#{FONT_NAME}'}</style>"
 	end
 end
