c166bff6ed76621e7923c476a1dd2498fdf01654
Paul Schaefer <yeppaul2@gmail.com>
2018-12-22 10:39:11 -0500

    Added helper function to create the proper preload tags, fixing #5 again

--- lib/google_fonts.rb
+++ lib/google_fonts.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'open-uri'
+require 'uri'
 
 # This helper makes it easier to change the Google Font included in the page.
 # Simply change it in data/global.json and this module does the rest.
@@ -20,16 +21,43 @@ module GoogleFontsHelper
   # Constructs the proper Google Fonts CSS URL
   STYLE_URL = "#{GOOGLE_APIS}#{FONT_NAME.tr(' ', '+')}:#{FONT_WEIGHT}".freeze
 
+  FILE_LOCATION = 'source/stylesheets/_google_fonts.scss'.freeze
+
   # Saves it as _google_fonts.scss for usage in Sass
-  IO.copy_stream(open(STYLE_URL), 'source/stylesheets/_google_fonts.scss')
+  IO.copy_stream(open(STYLE_URL), FILE_LOCATION)
+
+  # Opens file for reading
+  SCSS_FILE = File.open(FILE_LOCATION)
+
+  # Searches through file for URLs
+  FONT_URLS = URI.extract(SCSS_FILE.read, 'https')
+
+  # Loop over URLs...
+  FONT_URLS.each do |url|
+    # And remove that stray parenthesis...
+    url.tr!(')', '')
+  end
+
+  # Closes the file
+  SCSS_FILE.close
 
   # Constructs the class for Tachyons-like usage, and appends it to the file.
-  open('source/stylesheets/_google_fonts.scss', 'a') do |f|
+  open(FILE_LOCATION, 'a') do |f|
     f.puts ".#{FONT_NAME.parameterize}{font-family:'#{FONT_NAME}',sans-serif}"
   end
 
   # Outputs font name as class
   def google_font
     FONT_NAME.parameterize
+  end
+
+  # Combine all preload tags into a string, and return it to the page
+  def google_font_preload_tags
+    all_tags = ''
+    FONT_URLS.each do |url|
+      all_tags << "<link rel=\"preload\" href=\"#{url}\" as=\"font\" type=\"font/ttf\" crossorigin=\"anonymus\">\n"
+    end
+
+    all_tags
   end
 end
